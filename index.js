b3ec02bfc44effad9daff44a45db2b5af062e93d
Dmitry <df.creative@gmail.com>
2015-02-14 00:10:51 +0300

    Remove comments

--- index.js
+++ index.js
@@ -14,16 +14,7 @@ var evalString = require('./string');
  * Cumulatively eval AST, eval all possible evaluable things
  */
 function evalAst(node){
-
-	//FIXME: eval till no node has changed
-	// node = evalString(node);
-	// node = evalArray(node);
-	// node = evalString(node);
 	node = evalArray(node);
-
-	node = evalMath(node);
-	// node = evalExp(node);
-	// node = evalMath(node);
 	node = evalExp(node);
 	return node;
 }
